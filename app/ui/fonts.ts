f523d59527ddee94a329a27926a80d5f7715b13d
ruth1212 <ruthmalasilaban@gmail.com>
2026-04-15 14:04:30 +0700

    update

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,8 +1,14 @@
-import { Poppins } from "next/font/google";
+import { Poppins, Lusitana } from "next/font/google";
 
 export const poppins = Poppins({
   subsets: ["latin"],
   weight: ["300", "400", "500", "600", "700", "800"],
   variable: "--font-poppins",
   display: "swap",
+});
+
+// Tambahkan ini agar tidak error
+export const lusitana = Lusitana({
+  subsets: ["latin"],
+  weight: ["400", "700"],
 });
